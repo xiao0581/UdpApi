c33c91d1c6412434da668acb8eb5bdb0a695911a
xiaohui <xiaohui@XIAO-PC>
2024-04-04 11:22:36 +0200

    Add project files.

---
+++ UdpApi/Controllers/FotovognsController.cs
@@ -0,0 +1,60 @@
+﻿using FotovognRepositorylib;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UdpApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FotovognsController : ControllerBase
+    {
+        // GET: FotovognsController
+        private readonly IFotovognRepository _fotovognRepository;
+        public FotovognsController(IFotovognRepository fotovognRepository)
+        {
+            _fotovognRepository = fotovognRepository;
+        }
+
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpGet]
+        public ActionResult<IEnumerable<Fotovogn>> Get([FromQuery] string? nameFilter)
+        {
+            IEnumerable<Fotovogn> fotovogns   = _fotovognRepository.Get(null);
+            if (nameFilter != null)
+            {
+
+                fotovogns = _fotovognRepository.Get(nameFilter);
+
+            }
+            if (fotovogns.Any())
+            {
+                
+                return Ok(fotovogns);
+            }
+
+
+            else
+            {
+                return NoContent();
+            }
+
+        }
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [HttpPost]
+        public ActionResult<Fotovogn> Post([FromBody] Fotovogn fotovogn)
+        {
+
+                Fotovogn fotovogn1 = _fotovognRepository.AddFotovogn(fotovogn);
+            
+            return Created("/" + fotovogn1.Id, fotovogn1);
+
+        }
+        // POST: FotovognsController/Edit/5
+       
+
+        // GET: FotovognsController/Delete/5
+       
+    }
+}
---
+++ UdpApi/Program.cs
@@ -0,0 +1,26 @@
+using FotovognRepositorylib;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddSingleton<IFotovognRepository>(new FotovognRepository());
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
